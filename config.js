3acf60f24f727dd8b57db19d46e39a530b0b4752
Malavika007 <76998900+Malavika007@users.noreply.github.com>
2021-11-17 13:24:47 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+import 'firebase/auth'
+import 'firebase/database'
+import 'firebase/app'
+export const firebaseConfig = {
+  apiKey: "AIzaSyD-jTiHL335bXfJgQkRiPB8aU75ysMW2D0",
+  authDomain: "stroytelling-app.firebaseapp.com",
+  databaseURL: "https://stroytelling-app-default-rtdb.firebaseio.com",
+  projectId: "stroytelling-app",
+  storageBucket: "stroytelling-app.appspot.com",
+  messagingSenderId: "207321579369",
+  appId: "1:207321579369:web:c3805d527373a97c9f87f8"
+};
+
+// Initialize Firebase
+//const app = initializeApp(firebaseConfig);
